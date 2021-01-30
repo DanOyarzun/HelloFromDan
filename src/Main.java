2cd96f3e00a6de623758ce91698f493d16b621c3
pvfpinho <pvfpinho@hotmail.com>
2021-01-30 10:23:45 -0700

    "hello from Pam - 01/30"

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Dan");
+        System.out.println("Hello Dan, my name is Pam!");
     }
 }
